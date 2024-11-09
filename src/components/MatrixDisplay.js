02a4c70ba9dddef600205869bf4dfea47a946ff7
D. Hanga <121812202+d-hanga@users.noreply.github.com>
2024-11-09 16:34:56 +0100

    removed extra tab (not an error, but less clean code)

--- src/components/MatrixDisplay.js
+++ src/components/MatrixDisplay.js
@@ -1,30 +1,30 @@
 import "../style/MatrixDisplay.css";
 
 function MatrixDisplay({ name, matrix, buttonFunctioning, buttonName }) {
-        return (
-            <div className="full-matrix">
-                <div className="matrix-header">
-                    <div className="matrix-name">{name}</div>
-                    <div className="matrix-w-dim">
-                        <div className="matrix-dimension">
-                            {matrix.length}x{matrix[0].length}
-                        </div>
-                        <table className="matrix">
-                            <tbody>
-                            {matrix.map((row, i) => (
-                                <tr key={i}>
-                                {row.map((cell, j) => (
-                                    <td key={j}>{cell}</td>
-                                ))}
-                                </tr>
-                            ))}
-                            </tbody>
-                        </table>
+    return (
+        <div className="full-matrix">
+            <div className="matrix-header">
+                <div className="matrix-name">{name}</div>
+                <div className="matrix-w-dim">
+                    <div className="matrix-dimension">
+                        {matrix.length}x{matrix[0].length}
                     </div>
+                    <table className="matrix">
+                        <tbody>
+                        {matrix.map((row, i) => (
+                            <tr key={i}>
+                            {row.map((cell, j) => (
+                                <td key={j}>{cell}</td>
+                            ))}
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
                 </div>
-                <button className="matrix-something-button" onClick={buttonFunctioning}>{buttonName}</button>
             </div>
-        );
+            <button className="matrix-something-button" onClick={buttonFunctioning}>{buttonName}</button>
+        </div>
+    );
 }
 
 
